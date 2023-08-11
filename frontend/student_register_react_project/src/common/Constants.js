641ab7c322cd0dcdaeb89373b71d5895e6bc5d2e
hassan mirza <72553949+hassan0342@users.noreply.github.com>
2023-08-11 22:29:32 +0500

    fixes

--- frontend/student_register_react_project/src/common/Constants.js
+++ frontend/student_register_react_project/src/common/Constants.js
@@ -1,5 +1,5 @@
 // const base_url = 'http://localhost/test_stuednt_register_form/public/api';
-const base_url = 'http://localhost/backend/public/api';
+const base_url = 'http://localhost/students_registration_forms/backend/test_stuednt_register_form/public/api';
 export const Constant = {
     register_student: `${base_url}/register_student`,
     studentprofile: `${base_url}/studentprofile`,
